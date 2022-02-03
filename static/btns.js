dbb0697dee8baf9434eda71722576648bb8b12de
Gil Rubin <gilrub0@gmail.com>
2022-02-03 19:56:19 +0200

    added color change on while waiting server to responed

--- static/btns.js
+++ static/btns.js
@@ -1,43 +1,47 @@
-function userAction(button,action) {
+function userAction(button, action) {
+    document.getElementById(button).classList.toggle("waiting_for_response")
     var xhttp = new XMLHttpRequest();
     xhttp.open("POST", `/${action}`, true);
     xhttp.setRequestHeader("Content-type", "application/json");
     xhttp.send(`{"macro":"${button}"}`);
-    xhttp.onreadystatechange = function() { 
-        if (xhttp.readyState == 4){
-          if (xhttp.status == 204){
-            if  (action=="hold"){
-              toggleActive(button);
+    xhttp.onreadystatechange = function() {
+        if (xhttp.readyState == 4) {
+            if (xhttp.status == 204) {
+                document.getElementById(button).classList.toggle("waiting_for_response")
+                if (action == "hold") {
+                    toggleActive(button);
+                };
+                if (action == "release") {
+                    toggleActive(button);
+                };
+                if (action == "click") {
+                    toggleActive(button);
+                    setTimeout(function() { toggleActive(button); }, 50);
+                };
+                if (action == "releaseAll") {
+                    var active_buttons = document.getElementsByClassName("active")
+                    for (var i = 0; i < active_buttons.length; i++) {
+                        active_buttons[i].classList.replace("active", "not_active");
+                    };
+                    document.getElementById(button).style.backgroundColor = "";
+                };
+                if (action == "lock") {
+                    toggleActive(button);
+                };
             };
-            if (action=="release") {
-              toggleActive(button);
-            }; 
-            if (action=="click"){ 
-              toggleActive(button);
-              setTimeout(function(){toggleActive(button);},50);
-            };
-            if (action=="releaseAll"){
-              var active_buttons = document.getElementsByClassName("active")  
-              for (var i=0; i<active_buttons.length; i++) {
-                active_buttons[i].classList.replace("active","not_active");
-              };
-              document.getElementById(button).style.backgroundColor="";
-            };
-            if (action=="lock"){  
-              toggleActive(button);
-            };
-          };
         };
-      };
+    };
 }
 
 function loadColors() {
-  for (let item of document.getElementsByClassName("grid-item")){
-    item.classList.toggle("not_active")
-  };
+    for (let item of document.getElementsByClassName("grid-item")) {
+        item.classList.toggle("not_active")
+    };
 }
 
 function toggleActive(btn) {
-  document.getElementById(btn).classList.toggle("not_active");
-  document.getElementById(btn).classList.toggle("active");
+
+    document.getElementById(btn).classList.toggle("not_active");
+    document.getElementById(btn).classList.toggle("active");
+
 }
